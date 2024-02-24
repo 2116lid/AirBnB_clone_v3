3ecf77c5d324b738bbf4e108616f77a20eeebff3
Noah Alemayehu <noahlem21@gmail.com>
2024-02-24 18:43:41 +0000

    task4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -16,3 +16,22 @@ def stat():
     stat.status_code = 200
 
     return stat
+
+@app_views.route("/stats", methods=['GET'], strict_slashes=False)
+def status_obj():
+    """
+    stats of all objs route
+    """
+    data = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+    }
+
+    status_all = jsonify(data)
+    status_all.status_code = 200
+
+    return status_all
